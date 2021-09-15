1833924634ff293b5e4d6573936671e43b03f5f2
Tagliatelle <78481260+Tagliatellelol@users.noreply.github.com>
2021-09-15 18:12:59 +0400

    Added ability to add routes in subfolders

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,10 @@
-const fs = require("fs")
+const glob = require("glob");
 
-module.exports = function(app){
-    fs.readdirSync(__dirname).forEach(function(file) {
-        if (file == "index.js") return;
-        var name = file.substr(0, file.indexOf('.'));
-        const route = require('./' + name)
-                app.get(`/${route.name}`, async (req, res) => {
-                route.run(req, res)                    
-                })
-    });
+module.exports = function(app) {
+
+    glob("**/*.js", {cwd: __dirname}, (error, matches) => {
+        if (error) return;
+        if(matches === "index") return;
+        console.log(matches);
+    })
 }
